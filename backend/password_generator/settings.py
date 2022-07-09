5f97f52ecf4881cde2f85adf6b3fe87b2834fc2d
Morgan Firkins <mrjfirkins@gmail.com>
2022-07-09 20:09:08 +0100

    remove core

--- backend/password_generator/settings.py
+++ backend/password_generator/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "apps.core.middleware.RequestLogMiddleware",
 ]
 
 ROOT_URLCONF = "password_generator.urls"
