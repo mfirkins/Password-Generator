7e270808a0e1905c4d26e3ba6dc8f35a8f7813e2
Morgan Firkins <mrjfirkins@gmail.com>
2022-07-09 17:08:25 +0100

    changes

--- backend/password_generator/urls.py
+++ backend/password_generator/urls.py
@@ -1,7 +1,7 @@
-"""core URL Configuration
+"""password_generator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("passwordgenerated/", views.password, name="result"),
+    path("about/", views.about, name="about"),
 ]
