6dc1981a3e6264874baf295cc8d7764a319aa033
Morgan Firkins <mrjfirkins@gmail.com>
2022-07-11 10:29:27 +0100

    remove template settings

--- backend/password_generator/settings.py
+++ backend/password_generator/settings.py
@@ -58,23 +58,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "password_generator.urls"
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "backend.context_processors.template_vars",
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = "password_generator.wsgi.application"
 
 
