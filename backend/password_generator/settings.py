5382158714e2ed5909006b78fe6afddffb4d8e21
Morgan Firkins <mrjfirkins@gmail.com>
2022-07-11 10:41:26 +0100

    add templates back

--- backend/password_generator/settings.py
+++ backend/password_generator/settings.py
@@ -56,6 +56,27 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+            BASE_DIR / "account/templates",
+            BASE_DIR / "home/templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
 ROOT_URLCONF = "password_generator.urls"
 
 WSGI_APPLICATION = "password_generator.wsgi.application"
